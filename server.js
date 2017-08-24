c3dafbeed249e885fb509b4b5ef6d684bf29d1ed
Stephen Schools <stephen.schools>
2017-08-24 17:00:31 -0400

    need to do the edit and delete functions

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set("views", __dirname + "/views");
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.render("home");
